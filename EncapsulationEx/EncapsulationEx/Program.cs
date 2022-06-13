ec2bbd1022199d6d01d07b4603ee2c329e05734a
rajvi rajurkar <rajvi.vm@gmail.com>
2022-06-13 14:02:48 -0700

    initial commit

---
+++ EncapsulationEx/EncapsulationEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAcc = new BankAccount(400);
+
+            myAcc.Deposit(300);
+
+            var bal = myAcc.GetBalance();
+            Console.WriteLine($"Balance is {bal}");
+
+            myAcc.Withdraw(100);
+            Console.WriteLine($"Balance is {myAcc.GetBalance()}");
+
+        }
+    }
+}
